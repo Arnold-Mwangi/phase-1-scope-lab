47b2aee13ecf7f74a3a7e1f91ffaa3de37904182
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2023-06-16 11:22:15 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+var bestCustomer;
+const leastFavoriteCustomer = "Arnold";
+function upperCaseCustomerName (){
+    
+    return customerName =customerName.toUpperCase();
+}
+   
+function setBestCustomer(){
+   bestCustomer = "not bob"
+}
+console.log(setBestCustomer)
+
+function overwriteBestCustomer(){
+   bestCustomer = "maybe bob"
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Mwangi"
+}
